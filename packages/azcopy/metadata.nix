69ef0503d808d6562b04db76ee77a6030792ca2e
Cole Mickens <cole.mickens@gmail.com>
2020-07-25 00:37:15 -0700

    packages.azcopy: update

--- packages/azcopy/metadata.nix
+++ packages/azcopy/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/Azure/azure-storage-azcopy";
   branch = "master";
-  rev = "8fa65960b481165cfc30e38b16475d397f0d8a9c";
-  sha256 = "1157ps7dj6djdqdmdnbfkdkfah4325h7vb6pl2nh3imf4xxnc0l6";
+  rev = "be0e7b1665f7bb2171792f5e290ae8aed97236c6";
+  sha256 = "0bygbg1k6926ri3988wbz0b1vv6wamk799mn5nkjf0xa6gjfqqsr";
   vendorSha256 = "10bpzf8f7ibx1wzd0nzh5q1ynwfjr4n1gjygq4zqqxg51ganqj82";
 }
