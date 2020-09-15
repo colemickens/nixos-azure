c1eb5496e66fef2e2b8bf3289a2978ebf07fbf4e
Cole Mickens <cole.mickens@gmail.com>
2020-09-14 19:26:01 -0700

    improvements

--- packages/azure-linux-boot-agent/metadata.nix
+++ packages/azure-linux-boot-agent/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/colemickens/azure-linux-boot-agent";
   branch = "dev";
-  rev = "739ccd258c23205ccc639c4e015edffb417ce506";
-  sha256 = "sha256-sIflM0JHl57B4ZJ+nzCw0CXr0XasKrN2Ih0q0+96j/Y=";
-  cargoSha256 = "sha256-sUK4UnoZDdqpivQVnClwvWlo/6HGQLQJAkh9+7XjL50=";
+  rev = "60b7668e83870e96b284ced86a22f2bb48eb00c4";
+  sha256 = "sha256-2O0JQwkFvUb8zHIHl8fzKStlw6bJy4MqULgEQkJ1qOA=";
+  cargoSha256 = "sha256-U0Tkl8JQVoqI55QTtO9fxxR9CPg1bc2lI0Sm4d9joqA=";
 }
