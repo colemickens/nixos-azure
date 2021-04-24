b7a973b481c7f79e8764d4646966bb61bcd8524c
Cole Mickens <cole.mickens@gmail.com>
2021-04-24 23:20:28 +0000

    azutil: add deploy-vm, fixup upload-vhd

--- packages/azutil/default.nix
+++ packages/azutil/default.nix
@@ -4,7 +4,10 @@ runCommandNoCC "azutil-builder" {} ''
   mkdir -p $out/bin
 
   cp "${./upload-vhd}" $out/bin/upload-vhd
+  cp "${./deploy-vm}" $out/bin/deploy-vm
 
   sed -i 's|__BLOBXFER__|${blobxfer}/bin/blobxfer|g' $out/bin/upload-vhd
-  sed -i 's|__AZ__|${azure-cli}/bin/blobxfer|g' $out/bin/upload-vhd
+  sed -i 's|__AZ__|${azure-cli}/bin/az|g' $out/bin/upload-vhd
+
+  sed -i 's|__AZ__|${azure-cli}/bin/az|g' $out/bin/deploy-vm
 ''
