09294eb45024dfaa889de93671a3b56e1c689a33
Cole Mickens <cole.mickens@gmail.com>
2021-04-26 05:06:59 +0000

    fixup hash for rust

--- packages/azure-linux-boot-agent/metadata.nix
+++ packages/azure-linux-boot-agent/metadata.nix
@@ -3,5 +3,5 @@
   branch = "dev";
   rev = "60b7668e83870e96b284ced86a22f2bb48eb00c4";
   sha256 = "sha256-2O0JQwkFvUb8zHIHl8fzKStlw6bJy4MqULgEQkJ1qOA=";
-  cargoSha256 = "sha256-U0Tkl8JQVoqI55QTtO9fxxR9CPg1bc2lI0Sm4d9joqA=";
+  cargoSha256 = "sha256-98tdRfg88b2Wt2k3g5orvT9JKTOK5BQiJUogbQbuEMI=";
 }
